a62177a39b501bf7618cf1216831d7ce0a048fcc
marwa memmi <99818711+marwamemmi@users.noreply.github.com>
2023-11-15 19:23:19 +0100

    Update DevOpsProjectSpringBootApplication.java

--- src/main/java/tn/esprit/devops_project/DevOpsProjectSpringBootApplication.java
+++ src/main/java/tn/esprit/devops_project/DevOpsProjectSpringBootApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootApplication
-@ActiveProfiles("test")
 public class DevOpsProjectSpringBootApplication {
 
     public static void main(String[] args) {
